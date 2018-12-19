d9ca8548f7936ef95c72aeb5e2ce4d06645774d7
Dennis Evangelista <evangeli@usna.edu>
2018-12-19 14:15:28 -0500

    bleh

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
+/*
+ Nmea2kHeader encoding and decoding testing
+ */
+
 #include "mbed.h"
 #include "rtos.h"
 #include "sailbus.h"
+#include "stdio.h"
+
+Serial pc(USBTX,USBRX);
+Nmea2kHeader test(); 
 
 int main(void){
+  test.set_source(0x00);
+  pc.printf("test source is %d",test.get_source());
+
+  return 0;
 } // main()
